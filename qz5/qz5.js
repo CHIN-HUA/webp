cd59940476aa4da310eb9394005e0ee578d6cc6b
CHIN-HUA <tico9004040@gmail.com>
2022-04-13 17:30:10 +0800

    qz5

---
+++ qz5/qz5.js
@@ -0,0 +1,17 @@
+$("#test").on("click",function(){
+  
+  var myList = document.getElementById('myList');
+myList.addEventListener('click', function(e) {
+  if ( e.target.tagName.toLowerCase() === 'li' ) {
+    console.log(e.target.textContent);
+  }
+}, false);
+var newList = document.createElement('li');
+var textNode = document.createTextNode("hello cgu!");
+newList.appendChild(textNode);
+myList.appendChild(newList);
+
+});
+
+
+  
